9cb1882e566496dca0173c5bb034ac53d31fd2c1
cgarcia86 <58064023+cgarcia86@users.noreply.github.com>
2019-12-09 04:46:01 -0400

    Add project files.

---
+++ TestCode/Program.cs
@@ -0,0 +1,38 @@
+﻿using Nancy.Json;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Runtime.Serialization.Json;
+
+namespace TestCode
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            LstSuperheroes newHero = new LstSuperheroes();
+
+            string MsgData = "{ \"LstSuperheroes\": [ { \"Id\": 1, \"Name\": \"John Priest\" }, { \"Id\": 2, \"Name\": \"John Wick\" } ] }";
+
+            newHero.ParseProfileInfo(MsgData);
+
+        }
+    }
+
+    public class LstSuperheroes
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+
+
+        public void ParseProfileInfo(string msgData)
+        {
+            
+            var heroDetails = JsonConvert.DeserializeObject<List<LstSuperheroes>>(msgData);
+            
+        }
+
+    }
+
+}
